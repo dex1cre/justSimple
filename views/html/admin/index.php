37c9689b197ac4694d4986bb0278031102431549
dex1cre <dex1cre@mail.ru>
2017-03-12 19:42:23 +0300

    3 commit

---
+++ views/html/admin/index.php
@@ -0,0 +1,51 @@
+<?php
+#$q = $conn->query("SELECT * FROM tabs WHERE id_parents=0");
+?>
+<div class="container top-menu">
+	<ul>
+		<li><a href="тут она будет определяться">< (это ссылка назад)</a></li>
+		<li><a href="admin">home</a></li>
+		<li><a href="delete">удалить этот раздел</a></li>
+		<li><a href="logout">выйти</a></li>
+	</ul>
+</div>
+<div class="container">
+	<table>
+	<tr>
+		<th>#</th>
+		<th>Имя</th>
+		<th>Пароль</th>
+		<th>удалить</th>
+		<th>изменить</th>
+	</tr>
+	<tr>
+		<td>123</td>
+		<td>Sjdjdj</td>
+		<td>Abcd12345</td>
+		<td>удалить</td>
+		<td>изменить</td>
+	</tr>
+	<tr>
+		<td>123</td>
+		<td>Sjdjdj</td>
+		<td>Abcd12345</td>
+		<td>удалить</td>
+		<td>изменить</td>
+	</tr>
+	<tr>
+		<td>123</td>
+		<td>Sjdjdj</td>
+		<td>Abcd12345</td>
+		<td>удалить</td>
+		<td>изменить</td>
+	</tr>
+	<tr>
+		<td>123</td>
+		<td>Sjdjdj</td>
+		<td>Abcd12345</td>
+		<td>удалить</td>
+		<td>изменить</td>
+	</tr>
+	
+	</table>
+</div>
